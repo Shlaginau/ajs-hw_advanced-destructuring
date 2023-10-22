caf6244d4742e1832aec26dfc80105b58b9df4f4
Uliana Shlagina <shlaginau@gmail.com>
2023-10-22 23:33:44 +0700

    corrected

--- src/js/destructuringObject.js
+++ src/js/destructuringObject.js
@@ -1,15 +1,29 @@
 function extractSpecialAttacks({ special }) {
   return special.map((specialAtack) => {
     const {
-      id, name, description, icon,
+      id, name, description = 'Описание недоступно', icon,
     } = specialAtack;
     return {
       id,
       name,
-      description: description || 'Описание недоступно',
+      description,
       icon,
     };
   });
 }
 
 export default extractSpecialAttacks;
+
+/* function extractSpecialAttacks({ special }) {
+        return special.map((specialAtack) => {
+          const {
+            id, name, description, icon,
+          } = specialAtack;
+          return {
+            id,
+            name,
+            description: description || 'Описание недоступно',
+            icon,
+          };
+        });
+      } */
